7f5df96b98db04d1994b820b418f403a7af500c7
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-06-29 16:41:40 +0530

    page update

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,6 +1,7 @@
 import PreviousMap from "postcss/lib/previous-map";
 import React from "react";
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
+import Premier from "../components/Premier/Premier.component";
 
 const HomePage = () => {
  return (
@@ -8,6 +9,7 @@ const HomePage = () => {
     <div className="container mx-auto px-24 py-4">
       <h1 className="text-xl font-bold text-grey-800 my-3">The Best Of Entertainment</h1>
     <EntertainmentCardSlider/>
+    <Premier/>
     </div>
    
     </>
